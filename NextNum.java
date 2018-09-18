feb79fddd3d26714706e8085ef8fdfb1cc4d1a5c
Nandha kumar S <mailstonandhakumar@gmail.com>
2018-09-18 20:12:46 +0530

    Update and rename README.md to NextNum.java

---
+++ NextNum.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+  public class NextNum{
+    public static void main(String args[]){
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter Number:");
+      int a=sc.nextInt();
+      System.out.println("Next Number:"+(a+1));
+      }
+    }  
